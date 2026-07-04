45ddcb2d25f33c85c1ec50f4418a15dcff74a3bf
suil0304 <eagle030410@gmail.com>
2026-07-04 11:35:59 +0900

    #5.6

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-class Block {
+import crypto from "crypto";
 
+interface BlockShape {
+    hash:string;
+    prevHash:string;
+    height:number;
+    data:string;
+}
+
+class Block implements BlockShape {
+    public hash:string;
+
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data:string
+    ) {
+        this.hash = Block.calcHash(prevHash, height, data);
+    }
+
+    static calcHash(prevHash:string, height:number, data:string):string {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256")
+            .update(toHash)
+            .digest("hex");
+    }
 }
